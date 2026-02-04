bb57499a15dd728b5645abf628e7730b7d26c74a
Phat <phat.chauu26@gmail.com>
2026-02-03 18:51:30 -0800

    middleware: added token; auth-status check;

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { getSecret } = require('../services/secretsService');
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+
+            const secret = await getSecret('JWT_SECRET');
+            const decoded = jwt.verify(token, secret);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: 'Not authorized, no token' });
+    }
+};
+
+module.exports = { protect };
